e62974034535030227fe0bae8dffaf88de46dffc
jinjing <840536491@qq.com>
2019-04-10 00:35:28 +0800

    7

--- Apps/Wap/Controller/DataPlatformController.class.php
+++ Apps/Wap/Controller/DataPlatformController.class.php
@@ -9,10 +9,23 @@ class DataPlatformController extends Controller {
     protected $_account = 'shaoyang';
     protected $_app_id  = '780bbf7c7d094461b9d2982e66f803d7';
     protected $_app_key = '74b0a11a86a044a5b30bdade08218a50';
-    protected $_miguan_token_url = 'https://mi.juxinli.com/api/access_token';
-    protected $_miguan_search_url = 'https://mi.juxinli.com/api/search';
+
+    protected $_auth_url = 'https://viptest.juxinli.com/h5/authorize/';
+    //protected $_auth_url = 'https://vip.juxinli.com/h5/authorize/';
     
     function report() {
-        $this->display();
+        $get_data = [
+            'api_key' => $this->_app_key,
+            'callback_url' => 'http://58.87.110.104/data_platform/callback.html'
+        ];
+        $url = $this->_auth_url.'telecom?' . http_build_query($get_data);
+        header("Location: " . $url); 
+    }
+
+    function callback()
+    {
+        echo "<pre>";
+        var_dump($_POST, $_GET);
+        echo "</pre>";
     }
 }
