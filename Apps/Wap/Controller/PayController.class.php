8f4f0f231a02ced26a78882ce3b0e6ccd87b6c81
jinjing <840536491@qq.com>
2019-04-17 23:04:26 +0800

    37

--- Apps/Wap/Controller/PayController.class.php
+++ Apps/Wap/Controller/PayController.class.php
@@ -51,7 +51,7 @@ class PayController extends Controller {
         $input->SetTime_start(date("YmdHis"));
         $input->SetTime_expire(date("YmdHis", time() + 600));
         $input->SetGoods_tag("test");
-        $input->SetNotify_url("http://paysdk.weixin.qq.com/notify.php");
+        $input->SetNotify_url("http://www.zhixinrenapp.com/pay/notify_url");
         $input->SetTrade_type("JSAPI");
         $input->SetOpenid($openId);
         $config = new \WxPayConfig();
