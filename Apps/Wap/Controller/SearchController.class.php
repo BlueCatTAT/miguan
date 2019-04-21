cec83c0f1553d427fde2eb6d403d5198537a23a4
jinjing <840536491@qq.com>
2019-04-22 00:44:32 +0800

    fix

--- Apps/Wap/Controller/SearchController.class.php
+++ Apps/Wap/Controller/SearchController.class.php
@@ -87,6 +87,16 @@ class SearchController extends Controller {
         }
     }
 
+    function report_his()
+    {
+        $id = I('get.id');
+        $Search = M('Search');
+        $search_info = $Search->where(['id' => $id])->find();
+        $date = json_decode($search_info['data'], true);
+        $this->data = $data['data'];
+        $this->display('report');
+    }
+
     function search_log()
     {
         $uid = is_login();
