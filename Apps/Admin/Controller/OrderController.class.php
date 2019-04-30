fa6c82ba8f4947a8b90d533b56b1fa2b59b55d18
jinjing <840536491@qq.com>
2019-04-30 11:05:48 +0800

    86

--- Apps/Admin/Controller/OrderController.class.php
+++ Apps/Admin/Controller/OrderController.class.php
@@ -10,19 +10,56 @@ class OrderController extends RootController {
     
     public function order_list()
     {
-        $page = $_GET['page'] ? $_GET['page'] : 1;
-        
+        $Admin = M('Admin');
+        $Member = M('Member');
         $Order = M('Order');
+        
+        $page = $_GET['page'] ? $_GET['page'] : 1;
+
+        $where = ['a.status' => 2];
+        if (isset(I('start_time'))) {
+            $where[] = ['a.updated_time' => ['gt' => strtotime(I('start_time'))]]
+        }
+        if (isset(I('end_time'))) {
+            $where[] = ['a.updated_time' => ['lt' => strtotime(I('end_time')) + 3600 * 24]];
+        }
+        if (isset(I('trade_no'))) {
+            $where[] = ['a.trade_no' => I('trade_no')]
+        }
+        if (isset(I('aid'))) {
+            $agent_info = $Admin->where(['id' => I('aid')])->find();
+            $agent_list = $Admin->where(['pid' => I('aid')])->select();
+            if ($agent_list) {
+                $aid_list = array_column($agent_list, 'id');
+                $aid_list[] = I('aid');
+                $user_list = $Member->where(['aid' => ['in', $aid_list])->select();
+            } else {
+                $user_list = $Member->where(['aid' => I('aid')])->select();
+            }
+            $uid_list = array_column($user_list, 'id');
+            $where[] = ['a.uid' => ['in' => $uid_list]];
+        }
+
+        $agent_list = $Admin->where(['lv' => 1, 'status' => 1])->select();
+        $this->agent_list = $agent_list;
+
+        $total_fee = $Order->table('t_order a')
+                    ->field('a.*, b.nickname')
+                    ->join('t_member b on a.uid = b.id')
+                    ->where($where)
+                    ->sum('a.fee');
+        $this->total_fee = $total_fee;
+
         $order_list = $Order->table('t_order a')
             ->field('a.*, b.nickname')
             ->join('t_member b on a.uid = b.id')
-            ->where(['a.status' => 2])
+            ->where($where)
             ->order(['a.id' => 'desc'])
             ->page($page . ',10')
             ->select();
         $this->order_list = $order_list;
 
-        $count = $Order->where(['status' => 2])->count();
+        $count = $Order->where($where)->count();
 
         $this->count = $count;
         $this->page_count = ceil($count / 10);
