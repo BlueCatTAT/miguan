e04b67b50e3e0ee622f7e7af55227e0fd45ae6a7
jinjing <840536491@qq.com>
2019-05-03 23:48:09 +0800

    99

--- Apps/Wap/Controller/PublicController.class.php
+++ Apps/Wap/Controller/PublicController.class.php
@@ -34,6 +34,48 @@ class PublicController extends Controller {
         }
     }
 
+    public function test_msm()
+    {
+        $mobile = '13683582516';
+        $code = '888888';
+        $time = date('YmdHis');
+        $authorization = base64_encode($this->_accountid.':'.$time);
+        $sig=strtoupper(md5($this->_accountid.$this->_apisecret.$time)); 
+        
+        $data = [
+            "num" => $mobile,
+            "templateNum" => "5",
+            "var1" => $code 
+        ];
+
+        $header[] = "Accept: application/json";
+        $header[] = "Content-type: application/json;charset='utf-8'";
+        $header[] = "Content-Length: ".strlen( json_encode($data) );
+        $header[] = "Authorization: ".$authorization;
+
+        //帐号Id + 帐号APISecret +时间戳
+        $url = $this->_msm_url . '?sig=' . $sig;
+
+        $ch = curl_init ();
+        curl_setopt($ch, CURLOPT_URL, ($url));//地址
+        curl_setopt($ch, CURLOPT_POST, 1);   //请求方式为post
+        curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($data)); //post传输的数据。
+        curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER,$header);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); 
+        curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+        
+        $errno = curl_errno($ch);
+        $info  = curl_getinfo($ch);
+
+        $return = curl_exec($ch);
+        $return = json_decode($return, true);
+        echo "<pre>";
+        var_dump($return, $errno, $info);
+        echo "</pre>";
+    
+    }
+
     private function _send_msm($mobile, $code)
     {
         $time = date('YmdHis');
